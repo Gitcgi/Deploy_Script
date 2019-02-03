83af9ad720bbf3f576bbf463c53f5bae41c1965b
Ashok <ashokreddyjyothi37@gmail.com>
2019-02-03 18:40:27 +0000

    Modified Deploy script file

--- deploy_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-Version=$1
-ARTIFACTORY_USER=$2
-ARTIFACTORY_PASSWORD=$3
-echo "Downloading the file from artifactory"
-curl -o java-maven-junit-helloworld-${Version}.jar -u ${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD} http://40.117.86.242:8081/artifactory/libs-snapshot-local/java-maven-junit-helloworld-${Version}.jar
-if $?=0
-echo "Download complete..."
-if
--- deploy_script_new.sh
+++ deploy_script_new.sh
@@ -6,4 +6,4 @@ echo "Downloading the file from artifactory"
 curl -o java-maven-junit-helloworld-${Version}.jar -u ${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD} http://40.117.86.242:8081/artifactory/libs-snapshot-local/java-maven-junit-helloworld-${Version}.jar
 if $?=0
 echo "Download complete..."
-if
+fi
